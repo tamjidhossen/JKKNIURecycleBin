fbbf6a3f0307a75ce2854a5c5177df07f765bf9d
tamjidhossen <tamjidhossen0x@gmail.com>
2023-12-19 14:02:12 +0600

    Login Page Logo Added

---
+++ app/src/main/java/com/example/recyclebin/LoginOptionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclebin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_options);
+    }
+}
--- app/src/main/java/com/example/recyclebin/MainActivity.java
+++ app/src/main/java/com/example/recyclebin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.recyclebin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -52,23 +53,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showHomeFragment() {
+        //change toolbar textView text/title to Home
+        binding. toolbarTitleTv.setText ("Home");
 
+        //Show HomeFragment
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "HomeFragment");
+        fragmentTransaction.commit();
     }
-    private void showChatsFragment() {
-    }
-    private void showMyAdsFragment() {
-    }
-    private void showAccountFragment() {
+    private void showChatsFragment(){
+        //change toolbar textView text/title to Chats
+        binding. toolbarTitleTv.setText("Chats");
 
+        //Show ChatsFragment
+        ChatsFragment fragment = new ChatsFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "ChatsFragment");
+        fragmentTransaction.commit();
+    }
+    private void showMyAdsFragment(){
+        //change toolbar textView text/title to My Ads
+        binding.toolbarTitleTv.setText("My Ads");
+
+        //Show MyAdsFragment
+        MyAdsFragment fragment = new MyAdsFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "MyAdsFragment");
+        fragmentTransaction.commit();
+    }
+    private void showAccountFragment(){
+        //change toolbar textView text/title to Account
+        binding. toolbarTitleTv.setText ("Account");
+
+        //Show AccountFragment
+        AccountFragment fragment = new AccountFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(binding.fragmentsFl.getId(), fragment, "AccountFragment");
+        fragmentTransaction.commit();
     }
 }
 
 
-/*Steps
+/*Steps - 1
 * 1) Enable ViewBinding in build.gradle (optional: you may use findViewById too)
 * 2) Add Required Colors in colors.xml
-* 3) create menu_bottom.ml for Bottom Navigation Menu
-* 4) Add Bottom Navigation Menu UI in activity_main.ml
+* 3) create menu_bottom.xml for Bottom Navigation Menu
+* 4) Add Bottom Navigation Menu UI in activity_main.xml
 * 5) Handle Bottom Navigation Menu item click in MainActivity.java
 * 6) Create Required Fragments to show on clicking Bottom Navigation Menu
 * 7) Fragment Navigation
